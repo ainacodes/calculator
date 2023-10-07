2c0495c2fb1e2e57c4e91138a50cd22abe47c34e
ainacodes <noraina.nordin16@gmail.com>
2023-10-08 02:39:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# calculator
+
+## Standard Calculator
+
+- Only accept one operation at a time.
+
+### Project status
+
+- [x] Design
+- [x] Show the numbers when the button is clicked
+- [] add operation function
+- [] add keyboard support
---
+++ script.js
@@ -0,0 +1,38 @@
+const prevDisplay = document.querySelector('[data-prev-operand]');
+const currentDisplay = document.querySelector('[data-current-operand]');
+const numberButtons = document.querySelectorAll('[data-number');
+const operatorButtons = document.querySelectorAll('[data-operation');
+const equalsButton = document.querySelector('[data-equals]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+
+numberButtons.forEach((button) =>
+  button.addEventListener('click', () => appendNumber(button.textContent))
+);
+
+operatorButtons.forEach((button) =>
+  button.addEventListener('click', () => setOperation(button.textContent))
+);
+
+allClearButton.addEventListener('click', () => {
+  prevDisplay.textContent = '';
+  currentDisplay.textContent = '';
+});
+
+function appendNumber(number) {
+  if (currentDisplay.textContent.includes('.')) {
+    if (/^\d$/.test(number)) {
+      currentDisplay.textContent += number;
+    }
+  } else if (currentDisplay.textContent === '' && number === '.') {
+    currentDisplay.textContent = '0' + number;
+  } else {
+    currentDisplay.textContent += number;
+  }
+}
+
+function setOperation(operation) {
+  if (currentDisplay.textContent === '') return;
+  currentDisplay.textContent += operation;
+  prevDisplay.textContent = currentDisplay.textContent;
+}
